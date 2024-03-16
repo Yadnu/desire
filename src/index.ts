579b7fd786236be842a914b408c4c3c783b7b5b2
Yadneya Joshi <61934942+Yadnu@users.noreply.github.com>
2024-03-16 20:26:24 +0530

    adding changes

--- src/index.ts
+++ src/index.ts
@@ -25,8 +25,21 @@ export interface Env {
 	// MY_QUEUE: Queue;
 }
 
+
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		return new Response('Hello World!');
+		console.log(request.body);
+		console.log("<----------------------body ends here -------------------------------->");
+		console.log(request.headers);
+		
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
 	},
 };
